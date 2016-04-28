becb7d33f66109365e956e12946ae01e031cb7c7
lily <wq2a@mtmail.mtsu.edu>
2016-04-28 11:53:07 -0500

    fix bug

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # html
+
+### Configuration
+#### application/config/config.php
+$config['base_url'] = 'http://example.com/';//''; this is for mac 
--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1', //'localhost'
+	'hostname' => '127.0.0.1',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'ilife',
